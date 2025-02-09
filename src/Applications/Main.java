78ddf659f8477badd4538e5a24b35a0ef740349c
Hafid-stack <abdelhafid.idbahamd@gmail.com>
2025-02-09 10:40:31 +0100

    code works but still has increamenting number issue

--- src/Applications/Main.java
+++ src/Applications/Main.java
@@ -3,28 +3,60 @@ package Applications;
 import Exceptions.NaturalNumbers;
 import Exceptions.NegativeNumbersException;
 
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) throws NegativeNumbersException {
         try {
             NaturalNumbers n1=new NaturalNumbers(5);
             System.out.println("Initial Value " +n1.getValue());
-            n1.setValue(-50);
+           try {n1.setValue(-50);}
+           catch (NegativeNumbersException e) {
+               System.out.println(e.getMessage());
+
+           }
+            System.out.println("current value "+n1.getValue());
+           try {
+               n1.setValue(0);
+           }
+           catch (NegativeNumbersException e) {
+               System.out.println(e.getMessage());
+           }
             System.out.println("current value "+n1.getValue());
             for (int i=0;i<60;i++){
-                System.out.println("test loop for");
-                n1.increaseValue();
+
+               try {
+                   n1.increaseValue();
+               }catch (NegativeNumbersException e) {
+                   System.out.println(e.getMessage());
+break;
+               }
             }
+            System.out.println("test loop for");
             System.out.println("After increase value "+n1.getValue());
-            NaturalNumbers n2=new NaturalNumbers(-50);
-            System.out.println("Initial Value " +n2.getValue());
-            n2.setValue(50);
-            System.out.println("current value "+n2.getValue());
 
         }catch (NegativeNumbersException e){
             System.out.println(e.getMessage());
-            System.out.println("NegativeNumbersException "+e.getMessage()+e.negativeValue());
+            System.out.println("Message 1 + e.get1 "+e.getMessage()+e.negativeValue());
+        }
+        try {
+
+            NaturalNumbers n2=new NaturalNumbers(-60);
+            System.out.println("Initial Value " +n2.getValue());
+            try {
+                n2.setValue(50);
+            }catch (NegativeNumbersException e) {
+                System.out.println(e.getMessage());
+
+            }
+
+
+            System.out.println("current value "+n2.getValue());
+
+        }
+        catch (NegativeNumbersException e){
+            System.out.println(e.getMessage());
+            System.out.println("Message 2 + e.get2 "+e.getMessage()+e.negativeValue());
         }
 
     }
